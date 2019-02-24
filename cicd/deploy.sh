249534ad857bc7da99b57ae7a44fd2e6c1eac356
dupontm <martin.dupont@tngtech.com>
2019-02-24 15:44:04 +0100

    Revert "* consolidate build and deploy into one step"
    This reverts commit 2d27cfae

--- cicd/deploy.sh
+++ cicd/deploy.sh
@@ -2,4 +2,4 @@
 
 WEB_BUCKET=$(aws cloudformation describe-stack-resource --stack-name $DISTRIBUTION_STACK_NAME --logical-resource-id ContentBucket --output text --query StackResourceDetail.PhysicalResourceId)
 aws s3 sync ./build s3://$WEB_BUCKET/ --exclude index.html max-age=100000
-aws s3 cp ./build/index.html s3://$WEB_BUCKET/ --cache-control max-age=0
+aws s3 cp ./build/index.html s3://$WEB_BUCKET/ --cache-control max-age=20
